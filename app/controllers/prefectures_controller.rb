70dbf5cc4c5d40af14871cd66dbae58ff103c024
misakiohashi <omisaomisa1999@gmail.com>
2021-01-20 06:20:30 +0000

    for heroku

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,4 +1,6 @@
 class PrefecturesController < ApplicationController
+  before_action :admin_user,only: [:new,:create,:edit,:update,:destroy]
+  
   def index
     @prefectures1=Prefecture.find(1,2,3,4,5,6,7)
     @prefectures2=Prefecture.find(8,9,10,11,12,13,14)
