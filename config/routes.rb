88c52cb6e791f38af614f347750aba91dbb0b063
misakiohashi <omisaomisa1999@gmail.com>
2021-01-20 07:54:55 +0000

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'prefectures#index'
+  
+  
+  root to: 'users#new'
+  
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
