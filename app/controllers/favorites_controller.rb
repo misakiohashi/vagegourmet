6dc814ff458e43b1bdb2e813e6dba8db5c721183
misakiohashi <omisaomisa1999@gmail.com>
2021-01-22 13:32:39 +0000

    hidden_field_tag

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,14 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    
-    restaurant=Restaurant.find(params[:like_id])
+    restaurant = Restaurant.find(params["restaurant_id"])
     current_user.favorite(restaurant)
     flash[:success]='行きたいお店に登録しました'
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    
-    restaurant=Restaurant.find(params[:like_id])
+    restaurant = Restaurant.find(params["restaurant_id"])
     current_user.unfavorite(restaurant)
     flash[:success]='行きたいお店を解除しました'
     redirect_back(fallback_location: root_path)
